a5d31f2c1ea83f8e4a03de4bb5dbff33a0717ed0
Berke Balci <berkebalcii38@gmail.com>
2026-02-11 20:07:35 +0300

    fn real

--- app/src/main/java/com/example/venueexplorer/data/remote/VenueExplorerClient.kt
+++ app/src/main/java/com/example/venueexplorer/data/remote/VenueExplorerClient.kt
@@ -5,7 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object VenueExplorerClient {
     // Emulator için 10.0.2.2 kullanıyoruz
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "http://10.66.25.242:3000/"
+    //It works on the real device !!
 
     val apiService: VenueExplorerApiService by lazy {
         Retrofit.Builder()
